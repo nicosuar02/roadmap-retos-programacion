db194c56ed7a0ac51fd8bda0a2467950f97403bc
nicosuar02 <nicosuarauz@gmail.com>
2025-07-19 20:01:18 -0500

    Create nicosuar02.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/nicosuar02.py
@@ -0,0 +1,31 @@
+# https://python.org/
+# Comentario en una línea
+
+"""
+esto es un
+comentario
+en varias líneas
+"""
+
+'''
+esto tambien es
+comentario
+'''
+my_variable = "Mi variable"
+my_variable = "Nuevo valor variable"
+
+MY_CONSTANT = "Mi constante"  # POR CONVENCION
+MY_CONSTANT = "SDSASA"
+
+my_int = 1
+my_float = 2.5
+my_bool = True
+my_bool = False 
+my_string = " cadena de texto"
+my_other_string = "otra cadena"
+
+print ("Hola, Python")
+print (type(my_int))
+print (type(my_float))
+print (type(my_bool)) 
+print (type(my_string))
